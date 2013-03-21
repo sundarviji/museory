65140cb7a13bab8381c7df14d04ecad53ae6c2d1
raj <sun.muru@gmail.com>
2013-03-21 12:40:08 -0400

    a test and basic ideas

---
+++ src/psuedocode.java
@@ -0,0 +1,54 @@
+//
+//public class psuedocode {
+
+//}
+/*
+//model class will contain note/bar  data
+-------------------------------------------------------
+note class
+declare notes as private instances
+note contains A-G notes for treble
+                      “” for bass
+declare notevalue as a instance
+
+based on signatureTime generate a note value to a note
+
+//a bar of notes is based on the time signature
+----------------------------------------------------------
+bar class                       
+bar contains rules about how many notes can be on a bar based on the signature
+while bar is first
+generatesTime, a signature 4
+4 seconds is the total that can be in the bar
+   
+   call music_rules method
+
+
+//helper method that takes in a string of notevalue
+---------------------------------------------------------------
+music_rules method
+based on note class
+while all notes is equal to time signatures bar
+           go to next bar
+
+//generates a time signature
+--------------------------------------------------------------
+generatesTime
+instanstiate different time signatures
+make a constructor that gives them values
+put in a list
+randomize list
+
+----------------------------------------------------------------
+//viewer will contain the note look 
+                                     bar look---
+viewer class
+create a draw method
+
+create a main menu with labels and start gameplay
+-------------------------------------------------------------------
+add the notes onto the bar
+controller will tell the note what to do 
+                              -randomize the notes and pick it and put it as a note
+                                 put the notes on bar while music_rules is true
+*/
